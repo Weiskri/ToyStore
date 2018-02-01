5b3160ab463e3e99b1f1bf1b2f9a6d9cc15111fa
Weiskri <kkweis13@gmail.com>
2018-02-01 11:43:02 -0500

    initial commit

---
+++ ToyStore/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToyStore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] toyArray = { "action figure", "doll", "Legos" };
+            List<string> toyList = new List<string>() { "action figure", "stuffed animal", "Legos" };
+            toyList.Add("Barbie");
+            toyList.Add("remote control car");
+            toyList.Add("yo-yo");
+
+            foreach(string toy in toyList)
+            {
+                Console.WriteLine(toy);
+            }
+
+            //for (int i = 0; i <toyList.Count; i++) // does the same thing as the foreach loop, notice .Count instead of .Length!
+            //{
+            //    Console.WriteLine(toyList[i]);
+            //}
+
+       
+            
+            List<string> employeeNames = new List<string>(); // list size can change so no need to assign it a size.
+        }
+    }
+}
